8b0401334238eede41b5226e14980bd6d740b334
PibedeBarr10 <piotrwojtasiewicz8@gmail.com>
2020-05-26 18:56:17 +0200

    Set cookies

--- trash/views.py
+++ trash/views.py
@@ -10,4 +10,23 @@ def home(request):
     #user_language = 'pl' 
     #translation.activate(user_language)
     #request.session[translation.LANGUAGE_SESSION_KEY] = user_language
-    return render(request, 'home.html')
+    
+
+    #request.session.set_test_cookie()
+    #if request.session.test_cookie_worked():
+        #print("The test cookie worked!!!")
+        #request.session.delete_test_cookie()
+    
+    #return render(request, 'home.html')
+
+
+    response = render(request, 'home.html') # store the response in response variable
+    if not request.COOKIES.get('cookies_accept'):        
+        response.set_cookie('cookies_accept', 'false', 3600 * 24 * 365 * 2)
+
+    if not request.COOKIES.get('visits'):        
+        response.set_cookie('visits', '1', 3600 * 24 * 365 * 2)
+    else:
+        visits = int(request.COOKIES.get('visits', '1')) + 1
+        response.set_cookie('visits', str(visits),  3600 * 24 * 365 * 2)
+    return response
